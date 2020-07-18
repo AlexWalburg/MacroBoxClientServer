dace236fa9749802f75ab0d55c6b061532d1bd0b
alex walburg <ajwalburg@hotmail.com>
2020-07-17 18:07:10 -0700

    ofc it didn't commit everything

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -28,13 +28,399 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.textBox11 = new System.Windows.Forms.TextBox();
+            this.textBox12 = new System.Windows.Forms.TextBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.SaveConfig = new System.Windows.Forms.Button();
+            this.PortList = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.RefreshPorts = new System.Windows.Forms.Button();
+            this.TitleStuff = new System.Windows.Forms.LinkLabel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Reconnect = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel5.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel6.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(3, 178);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(802, 72);
+            this.panel1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(299, 29);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(276, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(26, 29);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(234, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(646, 26);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Save";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel2.Controls.Add(this.textBox3);
+            this.panel2.Controls.Add(this.textBox4);
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Location = new System.Drawing.Point(3, 256);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(802, 72);
+            this.panel2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(299, 29);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(276, 20);
+            this.textBox3.TabIndex = 3;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(26, 29);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(234, 20);
+            this.textBox4.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(646, 26);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Save";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // panel3
+            // 
+            this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel3.Controls.Add(this.panel5);
+            this.panel3.Controls.Add(this.textBox5);
+            this.panel3.Controls.Add(this.textBox6);
+            this.panel3.Controls.Add(this.button3);
+            this.panel3.Location = new System.Drawing.Point(3, 334);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(802, 72);
+            this.panel3.TabIndex = 5;
+            // 
+            // panel5
+            // 
+            this.panel5.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel5.Controls.Add(this.textBox9);
+            this.panel5.Controls.Add(this.textBox10);
+            this.panel5.Controls.Add(this.button5);
+            this.panel5.Location = new System.Drawing.Point(-2, 70);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(802, 72);
+            this.panel5.TabIndex = 5;
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(299, 26);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(276, 20);
+            this.textBox9.TabIndex = 3;
+            // 
+            // textBox10
+            // 
+            this.textBox10.Location = new System.Drawing.Point(26, 29);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(234, 20);
+            this.textBox10.TabIndex = 2;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(646, 26);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 1;
+            this.button5.Text = "Save";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(299, 29);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(276, 20);
+            this.textBox5.TabIndex = 3;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(26, 29);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(234, 20);
+            this.textBox6.TabIndex = 2;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(646, 26);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "Save";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // panel4
+            // 
+            this.panel4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel4.Controls.Add(this.textBox7);
+            this.panel4.Controls.Add(this.textBox8);
+            this.panel4.Controls.Add(this.button4);
+            this.panel4.Location = new System.Drawing.Point(3, 412);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(802, 72);
+            this.panel4.TabIndex = 5;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(299, 29);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(276, 20);
+            this.textBox7.TabIndex = 3;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(26, 29);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(234, 20);
+            this.textBox8.TabIndex = 2;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(646, 26);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 1;
+            this.button4.Text = "Save";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // panel6
+            // 
+            this.panel6.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel6.Controls.Add(this.textBox11);
+            this.panel6.Controls.Add(this.textBox12);
+            this.panel6.Controls.Add(this.button6);
+            this.panel6.Location = new System.Drawing.Point(3, 490);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(802, 72);
+            this.panel6.TabIndex = 6;
+            // 
+            // textBox11
+            // 
+            this.textBox11.Location = new System.Drawing.Point(299, 29);
+            this.textBox11.Name = "textBox11";
+            this.textBox11.Size = new System.Drawing.Size(276, 20);
+            this.textBox11.TabIndex = 3;
+            // 
+            // textBox12
+            // 
+            this.textBox12.Location = new System.Drawing.Point(26, 29);
+            this.textBox12.Name = "textBox12";
+            this.textBox12.Size = new System.Drawing.Size(234, 20);
+            this.textBox12.TabIndex = 2;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(646, 26);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 1;
+            this.button6.Text = "Save";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerSupportsCancellation = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // SaveConfig
+            // 
+            this.SaveConfig.Location = new System.Drawing.Point(651, 132);
+            this.SaveConfig.Name = "SaveConfig";
+            this.SaveConfig.Size = new System.Drawing.Size(75, 23);
+            this.SaveConfig.TabIndex = 7;
+            this.SaveConfig.Text = "Save Config";
+            this.SaveConfig.UseVisualStyleBackColor = true;
+            this.SaveConfig.Click += new System.EventHandler(this.SaveConfig_Click);
+            // 
+            // PortList
+            // 
+            this.PortList.FormattingEnabled = true;
+            this.PortList.Location = new System.Drawing.Point(13, 132);
+            this.PortList.Name = "PortList";
+            this.PortList.Size = new System.Drawing.Size(252, 21);
+            this.PortList.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 113);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "List Of Ports";
+            // 
+            // RefreshPorts
+            // 
+            this.RefreshPorts.Location = new System.Drawing.Point(287, 132);
+            this.RefreshPorts.Name = "RefreshPorts";
+            this.RefreshPorts.Size = new System.Drawing.Size(75, 23);
+            this.RefreshPorts.TabIndex = 10;
+            this.RefreshPorts.Text = "Refresh";
+            this.RefreshPorts.UseVisualStyleBackColor = true;
+            this.RefreshPorts.Click += new System.EventHandler(this.RefreshPorts_Click);
+            // 
+            // TitleStuff
+            // 
+            this.TitleStuff.AutoSize = true;
+            this.TitleStuff.Location = new System.Drawing.Point(10, 83);
+            this.TitleStuff.Name = "TitleStuff";
+            this.TitleStuff.Size = new System.Drawing.Size(149, 13);
+            this.TitleStuff.TabIndex = 11;
+            this.TitleStuff.TabStop = true;
+            this.TitleStuff.Text = "Formatting Guide For the Keys";
+            this.TitleStuff.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.TitleStuff_LinkClicked);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(517, 65);
+            this.label2.TabIndex = 12;
+            this.label2.Text = resources.GetString("label2.Text");
+            // 
+            // Reconnect
+            // 
+            this.Reconnect.Location = new System.Drawing.Point(369, 132);
+            this.Reconnect.Name = "Reconnect";
+            this.Reconnect.Size = new System.Drawing.Size(75, 23);
+            this.Reconnect.TabIndex = 13;
+            this.Reconnect.Text = "Reconnect";
+            this.Reconnect.UseVisualStyleBackColor = true;
+            this.Reconnect.Click += new System.EventHandler(this.Reconnect_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(800, 596);
+            this.Controls.Add(this.Reconnect);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.TitleStuff);
+            this.Controls.Add(this.RefreshPorts);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.PortList);
+            this.Controls.Add(this.SaveConfig);
+            this.Controls.Add(this.panel6);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Macro Client Side";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel5.ResumeLayout(false);
+            this.panel5.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.panel6.ResumeLayout(false);
+            this.panel6.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.TextBox textBox11;
+        private System.Windows.Forms.TextBox textBox12;
+        private System.Windows.Forms.Button button6;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button SaveConfig;
+        private System.Windows.Forms.ComboBox PortList;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button RefreshPorts;
+        private System.Windows.Forms.LinkLabel TitleStuff;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button Reconnect;
     }
 }
 
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -1,20 +1,202 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO.Ports;
 using System.Windows.Forms;
 
 namespace WindowsFormsApp1
 {
-    public partial class Form1 : Form
+    public partial class Form1 : Form 
     {
+        NotifyIcon notifyIcon;
+        String[][] keys = new String[5][];
+        bool[] doNext = new bool[5];
+        int i; int j;
+        public String port;
         public Form1()
         {
+            for(int i = 0; i < keys.Length; i++)
+            {
+                keys[i] = new String[2];
+                doNext[i] = false;
+            }
             InitializeComponent();
+            Resize += Form1_Resize;
+            notifyIcon = new NotifyIcon();
+            notifyIcon.BalloonTipText = "Smug";
+            notifyIcon.BalloonTipTitle = "Smug";
+            notifyIcon.Text = "Smug";
+            notifyIcon.Icon = Properties.Resources.Untitled;
+            notifyIcon.MouseClick+= notifyIcon_MouseDoubleClick;
+            backgroundWorker1.RunWorkerCompleted += BackgroundWorker1_RunWorkerCompleted;
+            this.Icon = Properties.Resources.Untitled;
+
+            if (System.IO.File.Exists("./config.txt"))
+            {
+                LoadFromFile();
+            } else
+            {
+                if (SerialPort.GetPortNames().Length > 0)
+                {
+                    port = SerialPort.GetPortNames()[0];
+                }
+                else
+                {
+                    port = "No ports found"; //comment
+                }
+            }
+            RefreshPorts_Click(null, null);
+            PortList.SelectedItem = port;
+
+            backgroundWorker1.RunWorkerAsync();
+        }
+
+        private void LoadFromFile()
+        {
+            String[] lines = System.IO.File.ReadAllLines("./config.txt");
+            port = lines[0];
+            for(int i = 1; i < lines.Length; i++)
+            {
+                keys[(i - 1) / keys[2].Length][(i - 1) % keys[0].Length] = lines[i];
+            }
+            textBox1.Text = lines[1];
+            textBox2.Text = lines[2];
+            textBox4.Text = lines[3];
+            textBox3.Text = lines[4];
+            textBox6.Text = lines[5];
+            textBox5.Text = lines[6];
+            textBox8.Text = lines[7];
+            textBox7.Text = lines[8];
+            textBox12.Text = lines[9];
+            textBox11.Text = lines[10];
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            keys[0][0] = textBox1.Text;
+            keys[0][1] = textBox2.Text;
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            keys[1][0] = textBox4.Text;
+            keys[1][1] = textBox3.Text;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            keys[2][0] = textBox6.Text;
+            keys[2][1] = textBox5.Text;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            keys[3][0] = textBox8.Text;
+            keys[3][1] = textBox7.Text;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            keys[4][0] = textBox12.Text;
+            keys[4][1] = textBox11.Text;
+        }
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                notifyIcon.Visible = true;
+                this.ShowInTaskbar = false;
+                notifyIcon.ShowBalloonTip(3000);
+            }
+        }
+
+        private void notifyIcon_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.WindowState = FormWindowState.Normal;
+            this.ShowInTaskbar = true;
+            notifyIcon.Visible = false;
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            BackgroundWorker worker = sender as BackgroundWorker;
+            SerialPort serialPort = new SerialPort(port, 9600);
+            serialPort.RtsEnable = false;
+            serialPort.Open();
+            while (true)
+            {
+                if (worker.CancellationPending ==true)
+                {
+                    e.Cancel = true;
+                    serialPort.Close();
+                    e.Result = "Restart";
+                    return;
+                }
+                else if (serialPort.BytesToRead > 0)
+                {
+                    int i = serialPort.ReadByte();
+                    serialPort.DiscardInBuffer();
+                    if (i > 4)
+                    {
+                        throw new InvalidEnumArgumentException("This is not the right port!");
+                    }
+                    else
+                    {
+                        SendKeys.SendWait(keys[i][doNext[i] ? 1 : 0]);
+                        doNext[i] = !doNext[i];
+                    }
+                }
+            }
+            
+        }
+        private void BackgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Error!=null)
+            {
+                MessageBox.Show(e.Error.Message + "\n\n" + "I reccomend finding the correct com port and then pressing the \"Reconnect\" button", "An error has occured with reading the Macro Box");
+            }
+            if (e.Cancelled)
+            {
+                backgroundWorker1.RunWorkerAsync();
+            }
+        }
+
+        private void TitleStuff_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            this.TitleStuff.LinkVisited = true;
+            System.Diagnostics.Process.Start("https://docs.microsoft.com/en-us/dotnet/api/system.windows.forms.sendkeys.send?view=netcore-3.1");
+        }
+
+        private void RefreshPorts_Click(object sender, EventArgs e)
+        {
+            PortList.Items.Clear();
+            foreach(String s in SerialPort.GetPortNames())
+            {
+                PortList.Items.Add(s);
+            }
+        }
+
+        private void Reconnect_Click(object sender, EventArgs e)
+        {
+            port = PortList.SelectedItem.ToString();
+            if (backgroundWorker1.IsBusy) backgroundWorker1.CancelAsync();
+           // while (backgroundWorker1.IsBusy) { } //stall until that changes;
+            
+            
+        }
+
+        private void SaveConfig_Click(object sender, EventArgs e)
+        {
+            using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"./config.txt"))
+            {
+                file.WriteLineAsync(port);
+                foreach (String[] ss in keys)
+                {
+                    foreach(String s in ss)
+                    {
+                        file.WriteLineAsync(s);
+                    }
+                }
+            }
         }
     }
 }
